919dac5d928ecefe7cf0daa207ec83be4dcd63a8
mehak714-labie <mehakaro714@gmail.com>
2026-03-04 22:23:43 +0530

    Add files via upload

---
+++ day15.c
@@ -0,0 +1,17 @@
+//Problem: Given a matrix, calculate the sum of its primary diagonal elements. The primary diagonal consists of elements where row index equals column index.
+#include <stdio.h>
+int main() {
+    int n, i, j, sum = 0;
+    printf("Enter order of square matrix: ");
+    scanf("%d", &n);
+    int matrix[n][n];
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);        }    }
+    // Calculating primary diagonal sum
+    for(i = 0; i < n; i++) {
+        sum += matrix[i][i];    }
+    printf("Sum of primary diagonal elements = %d", sum);
+    return 0;
+}
